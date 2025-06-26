13faac0567c714073c1fa52eafb7bf97a718eea6
imran2908 <imranchand2908@gmail.com>
2025-06-26 14:42:18 +0530

    Update main.tf
    use modules

--- main.tf
+++ main.tf
@@ -31,13 +31,13 @@ module "blog_sg" {
   version = "5.3.0"
   name    = "blog_new"
 
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
   
   ingress_rules       = ["http-80-tcp","http-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules        = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
