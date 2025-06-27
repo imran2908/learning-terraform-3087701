c861f6edbbf8878c685fa5a4aedbc9d76f7ef0a7
imran2908 <imranchand2908@gmail.com>
2025-06-27 11:50:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,11 +77,11 @@ module "alb" {
       }
     }
   ]
-  http_tcp_listeners = [
+  listeners = [
     {
       port               = 80
       protocol           = "HTTP"
-      target_group_index = 0 # Refers to the first target group (tg-http)
+      target_group_index = 0 # Your original index
     }
   ]
   
