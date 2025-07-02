f51dd6e09230f5b0aa0625fab9d0903823a788e6
imran2908 <imranchand2908@gmail.com>
2025-07-02 07:36:06 +0530

    Update main.tf
    create new VPC

--- main.tf
+++ main.tf
@@ -38,24 +38,6 @@ module "blog_vpc" {
 }
 
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
 resource "aws_instance" "blog" {
 
   ami           = data.aws_ami.app_ami.id
