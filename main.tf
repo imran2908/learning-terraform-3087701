65de4df4b5cf43a244db8e308891be35d1a00afe
imran2908 <imranchand2908@gmail.com>
2025-07-02 08:24:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,6 +56,8 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
   name    = "blog-alb"
+  load_balancer_type = "application"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
@@ -64,8 +66,8 @@ module "alb" {
     {
           
       name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol         = "HTTP"
+      backend_port             = 80
       target_type      = "instance"
         targets = {
           my_target = {
