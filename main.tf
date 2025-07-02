450bfcd7fcab98cc2be5c34fab07ebeba87d5071
imran2908 <imranchand2908@gmail.com>
2025-07-02 07:46:16 +0530

    Update main.tf
    load balancer

--- main.tf
+++ main.tf
@@ -52,6 +52,38 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.b.og_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      
+    }
+    
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Example"
+  }
+}
 
 
 module "blog_sg" {
