33fd3f89cd8e9a54a81a3b757268452914f87a25
imran2908 <imranchand2908@gmail.com>
2025-07-02 07:47:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.b.og_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     ex-http-https-redirect = {
